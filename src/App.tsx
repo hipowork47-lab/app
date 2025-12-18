d8d00b6f9a5072f13f2d08df27dc36e59588117b
hipowork47-lab <hipowork47@gmail.com>
2025-12-18 21:57:22 +0000

    Use HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { StoreProvider } from "@/store/store";
@@ -16,13 +16,13 @@ const App = () => (
       <Toaster />
       <Sonner />
       <StoreProvider>
-        {/* Use basename for GitHub Pages deployments */}
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        {/* Use HashRouter to work reliably on GitHub Pages */}
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </StoreProvider>
     </TooltipProvider>
   </QueryClientProvider>
