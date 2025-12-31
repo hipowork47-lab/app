51acd2eaf8c1c7492c2a78bf3662c03ddc1ef8e3
hipowork47-lab <hipowork47@gmail.com>
2025-12-31 18:18:05 +0000

    Localize create-account button

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -253,7 +253,7 @@ const Index = () => {
                   className="w-full sm:w-auto"
                   onClick={handleAddAccount}
                 >
-                  إنشاء حساب
+                  {t("createAccount")}
                 </Button>
               )}
               {/* اختيار اللغة */}
