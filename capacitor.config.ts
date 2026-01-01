6549ac619b244e94f77268cb6906c09361c8d360
hipowork47-lab <hipowork47@gmail.com>
2026-01-01 21:14:26 +0000

    Set mobile app name to POS

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.easypos.mobile",
-  appName: "Easy POS",
+  appName: "POS",
   webDir: "dist",
   server: {
     androidScheme: "https",
